810aa10e2a989750cc56cade2e269b427dc5e951
刘聪 <liucong@lvmenglings-Mac-mini.local>
2021-04-17 09:40:44 +0800

    modify var

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,16 @@ module.exports = {
               'lessOptions': {
                 javascriptEnabled: true,
                 modifyVars: {
-                  'primary-color': '#4E6EEB',
-                  'border-radius-base': '4px'
+                  'primary-color': '#4E6EEB', // 主色
+                  'text-color': '#2A3044', // 正文色
+                  'text-color-secondary': '#545762', // 副文本色，略浅
+                  'heading-color': '#192242', // 标题色，深色
+                  'border-color-base': '#E5E5F1', // 边框色
+                  'border-radius-base': '4px', // 基本圆角单位
+                  'select-selection-item-bg': '#f2f2f8', // select tag 背景色
+                  'modal-header-bg': '#f9fafc', // 模态框 header 背景色
+                  'label-color': '#545762', // 表单 label 使用副文本色
+                  'form-item-label-colon-margin-right': '16px' // form item label 冒号有右边距
                 }
               }
             },
@@ -35,6 +43,7 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       linkType: false,
+      filename: 'antd@4.13.1.1.css'
     }),
   ],
   optimization: {
